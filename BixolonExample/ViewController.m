6f8031c9b2cf2a011a9fa38b3c7027f9d01bab53
SWING1993 <songguohua10446627@gmail.com>
2018-12-12 15:07:33 +0800

    初始化

---
+++ BixolonExample/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BixolonExample
+//
+//  Created by 宋国华 on 2018/12/12.
+//  Copyright © 2018 songguohua. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
